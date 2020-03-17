893d7e1ba2ed1b35cfca3654bfa8bb96917ed8f0
Oscar Eduardo Pantoja Hernandez <xme6597@XME6597MAC.local>
2020-03-17 12:51:19 -0600

    [ADD]router articles

---
+++ src/router/articles.js
@@ -0,0 +1,113 @@
+const express = require('express')
+
+const article = require('../models/article')
+
+const router = express.Router()
+
+//router.use(auth)
+
+router.get('/', async (request, response) => {
+  try {
+    const allArticles = await article.getAll()
+    response.json({
+      success: true,
+      message: 'all articles',
+      data: {
+        allArticles
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+
+router.post('/', async (request, response) => {
+  try {
+    const newArticle = await article.create(request.body)
+    response.json({
+      success: true,
+      message: 'Article created',
+      data: {
+        article: newArticle
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+
+router.get('/:id', async (request, response) => {
+  try {
+    const { id } = request.params
+
+    const articleFound = await article.getById(id)
+
+    response.json({
+      success: true,
+      message: 'Article found',
+      data: {
+        article: articleFound
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+
+router.delete('/:id', async (request, response) => {
+  try {
+    const { id } = request.params
+
+    const articleDelete = await article.deleteById(id)
+
+    response.json({
+      success: true,
+      message: 'Article deleted',
+      data: {
+        article: articleDelete
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+
+router.patch('/:id', async (request, response) => {
+  try {
+    const { id } = request.params
+
+    const articleToUpdate = await article.updateById(id, request.body)
+
+    response.json({
+      success: true,
+      message: 'Article update',
+      data: {
+        article: articleToUpdate
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+
+module.exports = router
