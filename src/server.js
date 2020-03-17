211c4e599fd7e35aabffdd36b11622752fde3f0b
Oscar Eduardo Pantoja Hernandez <xme6597@XME6597MAC.local>
2020-03-17 13:06:39 -0600

    [ADD]server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
 //server
+const express = require('express')
+const cors = require('cors')
+
+const articlesRouter = require('./router/articles')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use('/articles', articlesRouter)
+
+module.exports = app
